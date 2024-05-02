1c9b4be080429b079ed229cd6416fb08cf833980
imisuchenko <xenoproxy@yandex.by>
2024-05-02 23:22:03 +0300

    * add Contacts

--- resources/js/components/Contacts.vue
+++ resources/js/components/Contacts.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="contacts" style="height: 400px;">
-    Contact Us
+  <div class="contacts">
+    <p class="contact-us">Contact Us</p>
+    <div class="container" style="height: auto;">      
+      <div class="contact-list">
+        <div class="contact-item"> 
+          <p class="contact-title">Address</p>
+          <p class="contact-content">A-311 Halla One and One Tower 101 Gasan Digital 2-ro, Geumcheon-gu, Seoul 08505 Korea Hyeyoung Lim </p>
+        </div>
+        <div class="contact-item">
+          <p class="contact-title">Email</p>  
+          <p class="contact-content">snptec@snptec.co.kr</p>
+        </div>
+        <div class="contact-item">
+          <p class="contact-title">Tel & Fax</p>
+          <p class="contact-content">+82 2 2101 2056</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
