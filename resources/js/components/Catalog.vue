421ca13c618179a5c68e3f727a203eae2926795d
imisuchenko <xenoproxy@yandex.by>
2024-05-02 23:21:48 +0300

    * Catalog list

--- resources/js/components/Catalog.vue
+++ resources/js/components/Catalog.vue
@@ -1,6 +1,44 @@
 <template>
-  <div class="products" style="height: 400px;">
-    Products
+  <div class="catalog" style="height: 400px;">
+    <div class="container" style="height: auto;">
+      <p class="catalog-title">Catalog</p>
+      <div class="catalog-list-container">
+        <ul class="catalog-list">
+            <li class="catalog-item">IGBT MODULES / THYRISTORS</li>
+            <li class="catalog-item">AUTOMATION</li>
+            <li class="catalog-item">ATTENUATORS</li>
+            <li class="catalog-item">BLOCKS</li>
+            <li class="catalog-item">VARISTORS</li>
+            <li class="catalog-item">FANS</li>
+            <li class="catalog-item">VIDEO EQUIPMENT</li>
+            <li class="catalog-item">GENERATORS / RESONATORS</li>
+            <li class="catalog-item">SENSORS</li>
+            <li class="catalog-item">ENGINES</li>
+            <li class="catalog-item">DIODES</li>
+            <li class="catalog-item">DISPLAYS</li>
+            <li class="catalog-item">INVERTERS</li>
+            <li class="catalog-item">INDUCTANCE</li>
+            <li class="catalog-item">CABLES</li>
+            <li class="catalog-item">CAPACITORS</li>
+            <li class="catalog-item">SILICON WAFERS</li>
+            <li class="catalog-item">MICROCIRCUITS</li>
+            <li class="catalog-item">OPTICS</li>
+            <li class="catalog-item">OPTOCOUPLES</li>
+            <li class="catalog-item">CIRCUIT BREAKERS</li>
+            <li class="catalog-item">BOARDS</li>
+            <li class="catalog-item">VOLTAGE CONVERTERS</li>
+            <li class="catalog-item">INDUSTRIAL MEMORY CARDS</li>
+            <li class="catalog-item">CONNECTORS / BUTTONS</li>
+            <li class="catalog-item">RESISTORS</li>
+            <li class="catalog-item">RELAY</li>
+            <li class="catalog-item">LEDs</li>
+            <li class="catalog-item">TRANSISTORS</li>
+            <li class="catalog-item">TRANSCEIVERS</li>
+            <li class="catalog-item">TRANSFORMERS</li>
+            <li class="catalog-item">FILTERS</li>
+        </ul>
+      </div>  
+    </div>
   </div>
 </template>
 
