060cdf54d11faf226b248d33b281caf6a632e9b7
imisuchenko <xenoproxy@yandex.by>
2024-05-02 23:22:13 +0300

    * Supply some info

--- resources/js/components/Supply.vue
+++ resources/js/components/Supply.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about" style="height: 400px;">
-    Supply
+  <div class="supply">
+    <div class="container" style="height: auto;">
+      <p>
+        SNP Tec Corp. engages in the supply of electronic components for the needs of manufacturers of electronic equipment and equipment of a wide range of profiles.
+        Our customers receive benefits when ordering parts from us:
+        100% confidence in product quality.
+        Affordable price.
+        Timely delivery.
+      </p>
+      <p>
+        SNP Tec Corp supplies components and materials for the production and repair of electronic equipment for any purpose.
+        Key Features:
+      </p>        
+        <ul class="supply-list">
+          <li>Flexible forms of payment</li>
+          <li>Short delivery times</li>
+          <li>Order from 1 piece</li>
+          <li>Checking the quality of supplied products</li>
+          <li>Delivery anywhere, worldwide</li>
+          <li>Technical support</li>
+        </ul>   
+    </div>
   </div>
 </template>
