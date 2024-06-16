c83c998acec71a49839beb46dd71a0520b035177
XenoProxy <xenoproxy@yandex.by>
2024-06-16 16:50:53 +0300

    supply design

--- resources/js/components/Supply.vue
+++ resources/js/components/Supply.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="supply">
-    <div class="container" style="height: auto;">
-      <p>
-        SNP Tec Corp. engages in the supply of electronic components for the needs of manufacturers of electronic equipment and equipment of a wide range of profiles.
-        Our customers receive benefits when ordering parts from us:
-        100% confidence in product quality.
-        Affordable price.
-        Timely delivery.
-      </p>
-      <p>
-        SNP Tec Corp supplies components and materials for the production and repair of electronic equipment for any purpose.
-        Key Features:
-      </p>        
-        <ul class="supply-list">
+    <div class="container supply-container" style="height: auto;">
+      <div class="supply-part text-justify">
+        <p style="font-size: 18px;">
+          SNP Tec Corp. engages in the supply of electronic components for the needs of manufacturers of electronic equipment and equipment of a wide range of profiles.
+          Our customers receive benefits when ordering parts from us:
+          100% confidence in product quality.
+          Affordable price.
+          Timely delivery.
+        </p>
+        <p style="font-size: 18px;">
+          SNP Tec Corp supplies components and materials for the production and repair of electronic equipment for any purpose.
+        </p>
+      </div>
+      <div class="supply-part">
+        <p style="font-size: 20px;"> Key Features: </p>        
+        <ul class="supply-list d-flex flex-column">
           <li>Flexible forms of payment</li>
           <li>Short delivery times</li>
           <li>Order from 1 piece</li>
           <li>Checking the quality of supplied products</li>
           <li>Delivery anywhere, worldwide</li>
           <li>Technical support</li>
-        </ul>   
+        </ul>  
+      </div>       
     </div>
   </div>
 </template>
