b2a88143ff1a3a58117f5f019c336065c8da7c93
XenoProxy <xenoproxy@yandex.by>
2024-06-20 16:21:19 +0300

    supply component translation

--- resources/js/components/Supply.vue
+++ resources/js/components/Supply.vue
@@ -2,26 +2,18 @@
   <div class="supply">
     <div class="container supply-container" style="height: auto;">
       <div class="supply-part text-justify supply-text">
-        <p style="font-size: 18px;">
-          SNP Tec Corp. engages in the supply of electronic components for the needs of manufacturers of electronic equipment and equipment of a wide range of profiles.
-          Our customers receive benefits when ordering parts from us:
-          100% confidence in product quality.
-          Affordable price.
-          Timely delivery.
-        </p>
-        <p style="font-size: 18px;">
-          SNP Tec Corp supplies components and materials for the production and repair of electronic equipment for any purpose.
-        </p>
+        <p style="font-size: 18px;">{{ $t('supply_text') }}</p>
+        <p style="font-size: 18px;">{{ $t('supply_p2') }}</p>
       </div>
       <div class="supply-part">
-        <p style="font-size: 20px;"> Key Features: </p>        
+        <p style="font-size: 20px;">{{ $t('supply_key_features') }}</p>        
         <ul class="supply-list d-flex flex-column">
-          <li>Flexible forms of payment</li>
-          <li>Short delivery times</li>
-          <li>Order from 1 piece</li>
-          <li>Checking the quality of supplied products</li>
-          <li>Delivery anywhere, worldwide</li>
-          <li>Technical support</li>
+          <li>{{ $t('supply_feature_1') }}</li>
+          <li>{{ $t('supply_feature_2') }}</li>
+          <li>{{ $t('supply_feature_3') }}</li>
+          <li>{{ $t('supply_feature_4') }}</li>
+          <li>{{ $t('supply_feature_5') }}</li>
+          <li>{{ $t('supply_feature_6') }}</li>
         </ul>  
       </div>       
     </div>
