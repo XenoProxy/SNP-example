6a485ff1c5e9ab8d844d057ba3b7789082213831
XenoProxy <xenoproxy@yandex.by>
2024-06-20 16:04:44 +0300

    about component translation

--- resources/js/components/About.vue
+++ resources/js/components/About.vue
@@ -3,20 +3,12 @@
     <div class="banner"></div>
     <div class="main-tex-container d-flex align-items-center justify-content-center">
       <p class="main-text text-center">
-        <span class="company-name">SNP Tec Corp.</span> is a company specializing in the assembly of SMT and DIP mounting components, as well as the creation of unique products in accordance with customer requirements.
-      </p>
+        <span class="company-name">{{ $t('about_company_name') }}</span>{{ $t('about_company_main') }}</p>
     </div>
     <div class="container" style="height: auto;">      
-      <p class="text-justify">
-        Expertise in the field of contract assembly of printed circuit boards allows us to offer solutions for the most demanding projects. SNP Tec Corp. has the experience and skills to perform both manual and automatic installation of SMD components. Using professional equipment and technology, SNP Tec Corp. guarantees the accuracy and reliability of every installation, including SMD reflow soldering and DIP installation, as well as double-sided assembly and multi-workpiece separation.
-      </p>
-      <p class="text-justify">
-        SNP Tec Corp. specializes in the assembly of products, from simple to high-tech, always adhering to customer requirements. SNP Tec Corp. creates prototypes and prototypes, develops and debugs software at the lower and upper levels. When packaging SNP Tec Corp. products. places special emphasis on their aesthetic and functional design, ensuring maximum protection and ease of use for the end user. Quality control is an integral part of every stage of production to ensure that each product meets the highest standards
-      </p>
-      <p class="text-justify">
-        Choosing SNP Tec Corp. for your contract electronics assembly, you have a reliable partner ready to offer customized and innovative solutions for your projects.
-        Take the first step towards success and contact SNP Tec Corp. today to learn more about our Contract PCB Assembly, Contract Circuit Board Assembly, and Contract Electronics Assembly services.
-      </p>     
+      <p class="text-justify">{{ $t('about_company_p1') }}</p>
+      <p class="text-justify">{{ $t('about_company_p2') }}</p>
+      <p class="text-justify">{{ $t('about_company_p3') }}</p>     
     </div>
     <hr class="about-hr">
   </div>
